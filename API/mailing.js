4819fdcb50c6f67b53e78a2a13eb4808c375b1b8
Erick <erick.teixeira_50@hotmail.com>
2020-03-27 21:45:33 -0300

    Sendgrid

--- API/mailing.js
+++ API/mailing.js
@@ -1,35 +1,24 @@
-const nodemailer = require('nodemailer');
+const sgMail = require('@sendgrid/mail');
 const cred = require('./credencials');
+
+sgMail.setApiKey(cred.sg_api_key);
 
 module.exports = {
 
-    sendToken(token, email) {
-        return new Promise((resolve, reject) => {
-            var transporter = nodemailer.createTransport({
-                service: 'gmail',
-                auth: {
-                  user: 'projetorastreadorcom241@gmail.com',
-                  pass: cred.email_password
-                },
-                tls: {rejectUnauthorized: false}
-            });
-              
-            var mailOptions = {
-                from: 'projetorastreadorcom241@gmail.com',
-                to: email,
-                subject: 'Seu Token para Recuperação de Senha',
-                text: `Use esse código para recuperar sua senha do Rastreador-Mobile:\n\n${token}`
-            };
-              
-            transporter.sendMail(mailOptions, function(error, info) {
-                if (error) {
-                  console.log(error);
-                  reject(error);
-                } else {
-                  //console.log('Email sent: ' + info.response);
-                  resolve(true);
-                }
-            });
-        });
-    }
+  sendToken(token, email) {
+    return new Promise((resolve, reject) => {
+      const msg = {
+        from: 'projetorastreadorcom241@gmail.com',
+        to: email,
+        subject: 'Seu Token de Recuperação de Senha',
+        text: `Utilize o código ${token} para redefinir sua senha do Rastreador-Mobile`,
+      };
+      try {
+        sgMail.send(msg);
+        resolve(true);
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
 }
